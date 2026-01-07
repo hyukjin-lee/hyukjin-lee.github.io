53909fccf1eff76cbe60be0ba417bb8dd38eb8cd
hyukjin-lee <gogokow27@gmail.com>
2026-01-07 23:13:33 +0900

    add contents

--- out/_next/static/DhbhxK8Wcgw3nzXRm8cvW/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fabout","\u002F","\u002Fblog","\u002Ftech","\u002Fdaily","\u002Fdaily\u002F[year]\u002F[month]\u002F[day]\u002F[slug]","\u002Ftech\u002F[year]\u002F[month]\u002F[day]\u002F[slug]","\u002Fblog\u002F[year]\u002F[month]\u002F[day]\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
